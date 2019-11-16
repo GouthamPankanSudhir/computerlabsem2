2e3eafbb452463e0a6daf0fee2b1017278ba043f
Goutham Pankan Sudhir <gouthampankan001@gmail.com>
2019-11-16 13:14:45 +0530

    Update

---
+++ auto.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+git add .
+git config --global user.name "Goutham Pankan Sudhir"
+git config --global user.email "gouthampankan001@gmail.com"
+git commit -m "Update"
+git push -u origin master
